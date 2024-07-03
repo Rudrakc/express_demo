4fc9b75e1d1afb62c6bf4a4c4e72e7309751b119
Rudrakc <rudrachauhan2888@gmail.com>
2024-07-03 10:49:28 +0530

    added CRUD

--- index.js
+++ index.js
@@ -18,17 +18,39 @@ let courses = [
   },
 ];
 
+
+// GET REQUEST
 app.get("/courses", (req, res) => {
   res.json(courses);
 });
 
+// POST REQUEST
 app.post("/courses", (req, res) => {
-    const data = req.body;
+    const data = {
+        id: courses.length+1,
+        name: req.body.name,
+    };
     console.log('Received data:', data);
     courses.push(data);
     console.log(courses);
     res.send('Data received');
 });
+
+// PUT REQUEST
+app.put("/courses/:id", (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send("Course id not found!!");
+    course.name = req.body.name;
+    res.json(course);
+});
+
+// DELETE REQUEST
+app.delete("/courses/:id", (req, res) => {
+    let courseId = courses.findIndex(c => c.id === parseInt(req.params.id));
+    if(courseId === -1) return res.status(404).send("Course id not found!!");
+    const deletedCourse = courses.splice(courseId, 1);
+    res.json(deletedCourse);
+})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000...");
